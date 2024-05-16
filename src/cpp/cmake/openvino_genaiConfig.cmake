8976cadda25691d298df8e0e343eee01a44616ac
Wovchena <vladimir.zlobin@intel.com>
2024-05-16 20:15:52 +0400

    alias

--- src/cpp/cmake/openvino_genaiConfig.cmake
+++ src/cpp/cmake/openvino_genaiConfig.cmake
@@ -1,4 +1,6 @@
 include(CMakeFindDependencyMacro)
 find_dependency(OpenVINO COMPONENTS Runtime)
 
-include("${CMAKE_CURRENT_LIST_DIR}/openvino_genaiTargets.cmake")
+if(NOT TARGET genai)
+    include("${CMAKE_CURRENT_LIST_DIR}/openvino_genaiTargets.cmake")
+endif()
